cc40e5d99805b1b6b379091f2dd12b4d651b8092
Clara(신상림) <sangrim.shin@dabeeo.com>
2023-02-09 21:12:01 +0900

    Changes to be committed:

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,48 @@
-import express, { Request, Response, NextFunction } from "express";
-import { fetchmap } from "./manager/network";
-const server = express();
-const port = 3000;
+import express, { Request, Response, NextFunction } from 'express';
+import { fetchmap } from './manager/network';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
-server.get("/", (req: Request, res: Response) => {
-  //   console.log(req);
-  console.log("received get command ");
-  res.send("Hello JS World!");
-  fetchmap(3);
+const app = express();
+const port = 8087;
+
+app.use(bodyParser.json()); // use() 를 통해 연결 시킨다! (사용하기 위해)
+
+app.get('/', (req: Request, res: Response) => {
+    //   console.log(req);
+    console.log('received get command ');
+    res.send('Hello JS World!');
+    fetchmap(3);
 });
 
-server.get("/api/user/:id", (req, res) => {
-  const { id } = req.params;
-  //   console.log(req);
-  console.log(req.params);
-  console.log(id);
-  res.json({ id });
+app.get('/api/user/:id', (req, res) => {
+    const { id } = req.params;
+    //   console.log(req);
+    console.log(req.params);
+    console.log(id);
+    res.json({ id });
 });
 
-server.post("/", (req, res) => {
-  console.log(req);
-  res.send("Hello World!");
+app.post('/v2/find-path', (req, res) => {
+    const { computingTypeList } = req.body;
+    const locations = req.body.locations;
+    console.log('received post command');
+
+    console.log(req.headers.authorization);
+    console.log(locations);
+    console.log(computingTypeList);
+    res.send('Hello World!');
+    if (req.headers.authorization) getMapAxios(req.headers.authorization);
 });
 
-server.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+async function getMapAxios(token: string) {
+    const response = await axios.get('https://api.dabeeomaps.com/v2/map?t=JS', {
+        headers: { 'Content-Type': 'application/json', 'Authorization': token },
+    });
+    const data = JSON.stringify(response.data);
+    console.log(data);
+}
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
 });
