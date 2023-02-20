2522c1f8ac90345c3ad8c94d16ea25ebb898465b
Clara(신상림) <sangrim.shin@dabeeo.com>
2023-02-20 20:29:27 +0900

    Changes to be committed:

--- src/test/app.test.ts
+++ src/test/app.test.ts
@@ -1,14 +1,19 @@
 import { Dollar } from '../Dollar';
 import { getPathRequest } from './getPathRequest';
 import { navigationOption } from './navigationOption';
-import { SingleFloorNoTypeNoWaypoints } from './singleFloorNoTypeNoWaypoints';
-import { DoubleFloorElevatorTypeNoWaypoints } from './doubleFloorElevatorTypeNoWaypoints';
-import { SingleFloorRecommendationType2Waypoints } from './singleFloorRecommendationType2Waypoints';
-import { DoubleFloorStairsType2Waypoints } from './doubleFloorStairsType2Waypoints';
-import { DoubleFloorElevatorType2Waypoints } from './doubleFloorElevatorType2Waypoints';
-import { NoResult } from './noResult';
-import { SingleFloorEscalatorType2Waypoints } from './singleFloorEscalatorType2Waypoints';
-import { DoubleFloorMultiType2Waypoints } from './doubleFloorMultiType2Waypoints';
+import { SingleFloorEscalatorType2Waypoints } from './result/singleFloorEscalatorType2Waypoints';
+import { SingleFloorNoTypeNoWaypoints } from './result/singleFloorNoTypeNoWaypoints';
+import { SingleFloorRecommendationType2Waypoints } from './result/singleFloorRecommendationType2Waypoints';
+import { DoubleFloorElevatorType2Waypoints } from './result/doubleFloorElevatorType2Waypoints';
+import { DoubleFloorElevatorTypeNoWaypoints } from './result/doubleFloorElevatorTypeNoWaypoints';
+import { DoubleFloorMultiType2Waypoints } from './result/doubleFloorMultiType2Waypoints';
+import { DoubleFloorStairsType2Waypoints } from './result/doubleFloorStairsType2Waypoints';
+import { DoubleFloorMulti2Type2Waypoints } from './result/doubleFloorMulti2Type2Waypoints';
+import { NoResult } from './result/noResult';
+import { Up } from './result/up';
+import { UpDown } from './result/upDown';
+import { Down } from './result/down';
+import { DownUp } from './result/downUp';
 
 const request = require('supertest');
 const dotenv = require('dotenv');
@@ -27,85 +32,130 @@ describe('JEST SAMPLE Test', () => {
 });
 
 describe('pathAPI Test', () => {
-    test('Single Floor No Type No waypoints', async () => {
-        const original = await getPathRequest(navigationOption.singleFloorNoTypeNoWaypoints);
-        // console.log(original);
-        expect(original).toStrictEqual(SingleFloorNoTypeNoWaypoints); // <- success
-    });
+    // test('Single Floor No Type No waypoints', async () => {
+    //     const original = await getPathRequest(navigationOption.singleFloorNoTypeNoWaypoints);
+    //     // console.log(original);
+    //     expect(original).toStrictEqual(SingleFloorNoTypeNoWaypoints); // <- success
+    // });
 
-    test('Single Floor Recommended Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.singleFloorRecommendationType2Waypoints);
+    // test('Single Floor Recommended Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.singleFloorRecommendationType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(SingleFloorRecommendationType2Waypoints, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(SingleFloorRecommendationType2Waypoints); // <- success
+    // });
+
+    // test('Single Floor Esclator Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.singleFloorEscalatorType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(SingleFloorEscalatorType2Waypoints, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(SingleFloorEscalatorType2Waypoints); // <- success
+    // });
+
+    // test('Double Floor Elevator Type No waypoints', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorElevatorTypeNoWaypoints);
+    //     const result = diff(DoubleFloorElevatorTypeNoWaypoints, original);
+    //     console.log(result);
+    //     expect(original).toStrictEqual(DoubleFloorElevatorTypeNoWaypoints); // <- success
+    // });
+
+    // test('Double Floor Stairs Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorStairsType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(DoubleFloorStairsType2Waypoints, original);
+    //     console.log(result);
+    //     expect(original).toStrictEqual(DoubleFloorStairsType2Waypoints); // <- success
+    // });
+
+    // test('Double Floor Elevator Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorElevatorType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(DoubleFloorElevatorType2Waypoints, original);
+    //     console.log(result);
+    //     expect(original).toStrictEqual(DoubleFloorElevatorType2Waypoints); // <- success
+    // });
+
+    // test('Double Floor Escalator Type with No Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorEscalatorTypeNoWaypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(NoResult, original);
+    //     console.log(result);
+    //     expect(original).toStrictEqual(NoResult); // <- success
+    // });
+
+    // test('Double Floor Escalator Type with 1 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorEscalatorType1Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(NoResult, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(NoResult); // <- success
+    // });
+
+    // test('Double Floor Escalator Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorEscalatorType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(NoResult, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(NoResult); // <- success
+    // });
+
+    // test('Double Floor Elevator Stairs Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorMultiType2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(DoubleFloorMultiType2Waypoints, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(DoubleFloorMultiType2Waypoints); // <- success
+    // });
+
+    // test('Double Floor Escalator Stairs Type with 2 Waypoints ', async () => {
+    //     const original = await getPathRequest(navigationOption.doubleFloorMulti2Type2Waypoints);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(DoubleFloorMulti2Type2Waypoints, original);
+    //     console.log(result);
+
+    //     expect(original).toStrictEqual(DoubleFloorMulti2Type2Waypoints); // <- success
+    // });
+
+    test('Up : 2층-> 11층 -> 11층 -> 11층', async () => {
+        const original = await getPathRequest(navigationOption.up);
         // console.log(JSON.stringify(original, null, 2));
-        const result = diff(SingleFloorRecommendationType2Waypoints, original);
+        const result = diff(Up, original);
         console.log(result);
 
-        expect(original).toStrictEqual(SingleFloorRecommendationType2Waypoints); // <- success
+        expect(original).toStrictEqual(Up); // <- success
     });
 
-    test('Single Floor Esclator Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.singleFloorEscalatorType2Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(SingleFloorEscalatorType2Waypoints, original);
-        console.log(result);
+    // test('Updown :  2층 -> 11층 -> 11층 -> 2층', async () => {
+    //     const original = await getPathRequest(navigationOption.upDown);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(UpDown, original);
+    //     console.log(result);
 
-        expect(original).toStrictEqual(SingleFloorEscalatorType2Waypoints); // <- success
-    });
+    //     expect(original).toStrictEqual(UpDown); // <- success
+    // });
 
-    test('Double Floor Elevator Type No waypoints', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorElevatorTypeNoWaypoints);
-        const result = diff(DoubleFloorElevatorTypeNoWaypoints, original);
-        console.log(result);
-        expect(original).toStrictEqual(DoubleFloorElevatorTypeNoWaypoints); // <- success
-    });
+    // test('Down : 11층 -> 11층 -> 2층 -> 2층', async () => {
+    //     const original = await getPathRequest(navigationOption.down);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(Down, original);
+    //     console.log(result);
 
-    test('Double Floor Stairs Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorStairsType2Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(DoubleFloorStairsType2Waypoints, original);
-        console.log(result);
-        expect(original).toStrictEqual(DoubleFloorStairsType2Waypoints); // <- success
-    });
+    //     expect(original).toStrictEqual(Down); // <- success
+    // });
 
-    test('Double Floor Elevator Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorElevatorType2Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(DoubleFloorElevatorType2Waypoints, original);
-        console.log(result);
-        expect(original).toStrictEqual(DoubleFloorElevatorType2Waypoints); // <- success
-    });
+    // test('DownUp : 11층 -> 11층 -> 2층 -> 11층', async () => {
+    //     const original = await getPathRequest(navigationOption.downUp);
+    //     // console.log(JSON.stringify(original, null, 2));
+    //     const result = diff(DownUp, original);
+    //     console.log(result);
 
-    test('Double Floor Escalator Type with No Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorEscalatorTypeNoWaypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(NoResult, original);
-        console.log(result);
-        expect(original).toStrictEqual(NoResult); // <- success
-    });
-
-    test('Double Floor Escalator Type with 1 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorEscalatorType1Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(NoResult, original);
-        console.log(result);
-
-        expect(original).toStrictEqual(NoResult); // <- success
-    });
-
-    test('Double Floor Escalator Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorEscalatorType2Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(NoResult, original);
-        console.log(result);
-
-        expect(original).toStrictEqual(NoResult); // <- success
-    });
-
-    test('Double Floor Elevator Stairs Type with 2 Waypoints ', async () => {
-        const original = await getPathRequest(navigationOption.doubleFloorMultiType2Waypoints);
-        // console.log(JSON.stringify(original, null, 2));
-        const result = diff(DoubleFloorMultiType2Waypoints, original);
-        console.log(result);
-
-        expect(original).toStrictEqual(DoubleFloorMultiType2Waypoints); // <- success
-    });
+    //     expect(original).toStrictEqual(DownUp); // <- success
+    // });
 });
--- src/test/navigationOption.ts
+++ src/test/navigationOption.ts
@@ -181,4 +181,110 @@ export const navigationOption = {
             },
         ],
     },
+    doubleFloorMulti2Type2Waypoints: {
+        computingTypeList: ['ESCALATOR', 'STAIRS'],
+        locations: [
+            {
+                poiId: 'PO-4JvSQCWHC2270', // 남자화장실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-bG8eepPeB2502', // 여자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+            {
+                poiId: 'PO-M02DvTVjp8449', // 회의실1 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+        ],
+    },
+
+    up: {
+        locations: [
+            {
+                poiId: 'PO-bG8eepPeB2502', // 여자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-WgCv1-qBo8094', // 사업전략부 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+        ],
+        computingTypeList: ['RECOMMENDATION'],
+    },
+    upDown: {
+        locations: [
+            {
+                poiId: 'PO-bG8eepPeB2502', // 여자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-WgCv1-qBo8094', // 사업전략부 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-9InVzIGv20417', // 남자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+        ],
+        computingTypeList: ['RECOMMENDATION'],
+    },
+    down: {
+        locations: [
+            {
+                poiId: 'PO-4JvSQCWHC2270', // 남자화장실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-9InVzIGv20417', // 남자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+            {
+                poiId: 'PO-bG8eepPeB2502', // 여자화장실 (2층. 모의주행 중 층변경 테스트 목적)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+        ],
+        computingTypeList: ['RECOMMENDATION'],
+    },
+    downUp: {
+        locations: [
+            {
+                poiId: 'PO-4JvSQCWHC2270', // 남자화장실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-NMvw3E0pe1690', // 플랫폼사업부 회의실 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+            {
+                poiId: 'PO-bG8eepPeB2502', // 여자화장실 (2층)
+                floorId: 'FL-vf3q07spbmsw8132',
+            },
+            {
+                poiId: 'PO-M02DvTVjp8449', // 회의실1 (11층)
+                floorId: 'FL-t4vqgyek3jnb8146',
+            },
+        ],
+        computingTypeList: ['RECOMMENDATION'],
+    },
 };
